28b8f7242004c88151d4f1fd99350e2a5692418d
Anastasie Janot <anastasie.janot@gmail.com>
2024-01-18 19:33:03 +0100

    FIX??

--- 0_DataPreparation/preparation.R
+++ 0_DataPreparation/preparation.R
@@ -1,6 +1,7 @@
 # drei csv zusammenführen als tibble
 # Funktionen aus skimR und DataExplorer nutzen, um Daten in Tibble genauer anzusehen
 
+# Laden der Libraries
 library(tidyverse)
 library(skimr)
 library(DataExplorer)
@@ -8,13 +9,14 @@ library(dplyr)
 library(lubridate)
 library(readr)
 
-
+# Einlesen der Daten
 kiwo <- read.csv("0_DataPreparation/kiwo.csv")
 wetter <- read.csv("0_DataPreparation/wetter.csv")
 umsatz <- read.csv("0_DataPreparation/train.csv")
 feiertage <- read.csv("0_DataPreparation/Feiertage.csv", sep = ";")
 schulferien <- read.csv("0_DataPreparation/Schulferien.csv")
 
+# Sortieren der Datumsangaben nach Jahr, Monat und Tag
 umsatz$Datum <- ymd(umsatz$Datum)
 wetter$Datum <- ymd(wetter$Datum)
 kiwo$Datum <- ymd(kiwo$Datum)
@@ -22,11 +24,11 @@ kiwo$Datum <- ymd(kiwo$Datum)
 # eigene Variablen
 schulferien$Datum <- ymd(schulferien$Datum)
 
-# die Feiertage sind als dd.mm.yyyy gespeichert...
+# die Feiertage sind als dd.mm.yyyy gespeichert, sollen aber als mm-dd-yyyy gespeichert werden
 feiertage$Datum <- as.Date(feiertage$Datum, format = "%d.%m.%Y")
-# sollen aber als mm-dd-yyyy gespeichert werden
 feiertage$Datum_neues_format <- format(feiertage$Datum, "%m-%d-%Y")
 
+#Zusammenführen der Daten als Tibble und speichern in einer CSV-Datei
 merged_data <- umsatz %>%
   left_join(wetter, by = "Datum") %>%
   left_join(kiwo, by = "Datum") %>%
@@ -36,6 +38,7 @@ merged_data <- umsatz %>%
 ergebnis_tibble <- as_tibble(merged_data)
 write.csv(ergebnis_tibble, file="0_DataPreparation/joinedData.csv")
 
+#Erstellen eines linearen Models
 mod <- lm(Umsatz ~ as.factor(Warengruppe)+Temperatur+KielerWoche, ergebnis_tibble)
 summary(mod)
 

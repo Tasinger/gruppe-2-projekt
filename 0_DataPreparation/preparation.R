c31545ae6cddf521af8920bc80a9c77ebb976bff
Anastasie Janot <anastasie.janot@gmail.com>
2024-01-18 18:26:17 +0100

    [ADD] Added Feiertage & Ferien, adjusted code accordingly

--- 0_DataPreparation/preparation.R
+++ 0_DataPreparation/preparation.R
@@ -1,8 +1,6 @@
 # drei csv zusammenführen als tibble
 # Funktionen aus skimR und DataExplorer nutzen, um Daten in Tibble genauer anzusehen
 
-#TODO: Feiertage hinzufügen + weitere zusätzliche Variabel
-
 library(tidyverse)
 library(skimr)
 library(DataExplorer)
@@ -14,15 +12,24 @@ library(readr)
 kiwo <- read.csv("0_DataPreparation/kiwo.csv")
 wetter <- read.csv("0_DataPreparation/wetter.csv")
 umsatz <- read.csv("0_DataPreparation/train.csv")
+feiertage <- read.csv("0_DataPreparation/Feiertage.csv")
+schulferien <- read.csv("0_DataPreparation/Schulferien.csv")
 
 umsatz$Datum <- ymd(umsatz$Datum)
 wetter$Datum <- ymd(wetter$Datum)
 kiwo$Datum <- ymd(kiwo$Datum)
 
-data <- left_join(umsatz,kiwo,by = "Datum")
-ergebnis <- left_join(data,wetter, by= "Datum")
+# eigene Variablen
+feiertage$Datum <- ymd(feiertage$Datum)
+schulferien$Datum <- ymd(schulferien$Datum)
 
-ergebnis_tibble <- as_tibble(ergebnis)
+merged_data <- umsatz %>%
+  left_join(wetter, by = "Datum") %>%
+  left_join(kiwo, by = "Datum") %>%
+  left_join(feiertage, by = "Datum") %>%
+  left_join(schulferien, by = "Datum")
+
+ergebnis_tibble <- as_tibble(merged_data)
 write.csv(ergebnis_tibble, file="0_DataPreparation/joinedData.csv")
 
 mod <- lm(Umsatz ~ as.factor(Warengruppe)+Temperatur+KielerWoche, ergebnis_tibble)
